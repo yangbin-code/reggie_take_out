88c4ec31cca80e022e948484969c1e0fcc547bde
yangbin <3097137090@qq.com>
2022-09-23 23:18:07 +0800

    init

---
+++ src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.itheima.reggie.common;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie.entity.Category;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+
+/*
+*  全局异常处理
+* */
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@ResponseBody
+@Slf4j
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        log.error(ex.getMessage());
+        if (ex.getMessage().contains("Duplicate entry")) {
+            String[] split = ex.getMessage().split(" ");
+            String msg = split[2] + "已存在";
+            return R.error(msg);
+        }
+        return R.error("失败了");
+
+    }
+
+}
---
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -0,0 +1,42 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+    @Resource
+    private CategoryService categoryService;
+
+    @PostMapping
+    public R<String> save(@RequestBody Category category) {
+        log.info("category:{}",category);
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize) {
+        Page<Category> pageInfo = new Page<>(page,pageSize);
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByAsc(Category::getSort);
+        categoryService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
+    }
+
+    @DeleteMapping
+    public R<String> delete(Long ids) {
+        log.info("删除分类，id为：{}",ids);
+        categoryService.removeById(ids);
+        return R.success("分类信息删除成功");
+    }
+}
